7a5043307fb7527829d69bcc5f02a4bce6cf0bbd
anis.hannachi <anishannachi@debian>
2021-10-27 13:04:33 +0200

    complete

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,28 @@
-export default function Home({posts}) {
+import Link from "next/link";
+
+export default function Home({ posts }) {
   return (
     <div>
-      {posts && posts.map((post)=>(
-        <div key={post.id}>
-          <h2>{post.Title}</h2>
-          <div>{post.User.username}</div>
-        </div>
-      ))}
+      {/* loop over the posts and show them */}
+      {posts &&
+        posts.map((post) => (
+          <Link href={`/${post.Slug}`} key={post.id}>
+            <a>
+              <h2>{post.Title}</h2>
+              <div>{post.User.username}</div>
+            </a>
+          </Link>
+        ))}
     </div>
-  )
+  );
 }
 
-export async function getStaticProps(){
-  //get posts from our api
-  const res = await fetch('http://localhost:1337/posts');
+export async function getStaticProps() {
+  // get posts from our api
+  const res = await fetch("http://localhost:1337/posts");
   const posts = await res.json();
 
-  return{
-    props: {posts},
-  }
+  return {
+    props: { posts },
+  };
 }
